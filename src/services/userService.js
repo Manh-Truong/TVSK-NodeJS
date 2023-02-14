f40861128ed011814f9c7543b103412545b3f3e3
Manh-Truong <truonghuymanh20@gmail.com>
2023-02-14 21:28:25 +0700

    first commit

---
+++ src/services/userService.js
@@ -0,0 +1,236 @@
+import db from "../models/index";
+import bcrypt from "bcryptjs";
+
+const salt = bcrypt.genSaltSync(10);
+
+let hashUserPassword = (password) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      //lưu ý, truyền vào đúng password cần hash
+      // let hashPassWord = await bcrypt.hashSync("B4c0/\/", salt); => copy paste mà ko edit nè
+      let hashPassWord = await bcrypt.hashSync(password, salt);
+
+      resolve(hashPassWord);
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+let handleUserLogin = (email, password) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let userData = {};
+      let isExist = await checkUserEmail(email);
+      if (isExist) {
+        //user already exist
+        let user = await db.User.findOne({
+          attributes: ["email", "roleId", "password", "firstName", "lastName"],
+          where: { email: email },
+          raw: true,
+        });
+        if (user) {
+          //compare password: dùng cách 1 hay cách 2 đều chạy đúng cả =))
+          //  Cách 1: dùng asynchronous (bất đồng bộ)
+          let check = await bcrypt.compare(password, user.password);
+
+          // Cách 2: dùng synchronous  (đồng bộ)
+          // let check = bcrypt.compareSync(password, user.password);
+
+          if (check) {
+            userData.errCode = 0;
+            userData.errMessage = "OK";
+
+            delete user.password;
+            userData.user = user;
+          } else {
+            userData.errCode = 3;
+            userData.errMessage = "Mật khẩu sai";
+          }
+        } else {
+          userData.errCode = 2;
+          userData.errMessage = `Người dùng không được tìm thấy`;
+        }
+      } else {
+        //return error
+        userData.errCode = 1;
+        userData.errMessage = `Email của bạn không tồn tại trong hệ thống của chúng tôi, vui lòng thử email khác`;
+      }
+      resolve(userData);
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+let checkUserEmail = (userEmail) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let user = await db.User.findOne({
+        where: { email: userEmail },
+      });
+      if (user) {
+        resolve(true);
+      } else {
+        resolve(false);
+      }
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+let getAllUsers = (userId) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let users = "";
+      if (userId === "ALL") {
+        users = await db.User.findAll({
+          attributes: {
+            exclude: ["password"],
+          },
+        });
+      }
+      if (userId && userId !== "ALL") {
+        users = await db.User.findOne({
+          where: { id: userId },
+          attributes: {
+            exclude: ["password"],
+          },
+        });
+      }
+
+      resolve(users);
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+let createNewUser = (data) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      // check email có tồn tại không
+      let check = await checkUserEmail(data.email);
+      if (check === true) {
+        resolve({
+          errCode: 1,
+          errMessage:
+            "Email của bạn đã được sử dụng, vui lòng thử một email khác!",
+        })
+      }else {
+        let hashPassWordFromBcrypt = await hashUserPassword(data.password);
+        await db.User.create({
+          email: data.email,
+          password: hashPassWordFromBcrypt,
+          firstName: data.firstName,
+          lastName: data.lastName,
+          address: data.address,
+          phonenumber: data.phonenumber,
+          gender: data.gender === "1" ? true : false,
+          roleId: data.roleId,
+      });
+      resolve({
+        errCode: 0,
+        message: "OK",
+      })
+      }
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+let deleteUser = (userId) => {
+  return new Promise(async (resolve, reject) => {
+    let foundUser = await db.User.findOne({
+      where: { id: userId },
+    });
+    if (!foundUser) {
+      resolve({
+        errCode: 2,
+        errMessage: "Đã sử dụng không tồn tại",
+      });
+    }
+
+    await db.User.destroy({
+      where: { id: userId },
+    });
+
+    resolve({
+      errCode: 0,
+      errMessage: "Đã sử dụng bị xóa",
+    });
+  });
+};
+
+let updateUserData = (data) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+        if(!data.id){
+            resolve({
+                errCode: 2,
+                errMessage: 'Thiếu thông số bắt buộc'
+            })
+        }
+        let user = await db.User.findOne({
+            where: { id: data.id },
+            raw: false,
+        })
+        console.log('check user', data);
+        if (user) {
+            user.firstName = data.firstName;
+            user.lastName = data.lastName;
+            user.address = data.address;
+            user.phonenumber = data.phonenumber;
+
+          await user.save();
+
+        resolve({
+          errCode: 0,
+          message: "Cập nhật người dùng thành công!",
+        })
+      } else {
+        resolve({
+          errCode: 1,
+          errMessage: `Không tìm thấy người dùng`,
+        });
+      }
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+let getAllCodeService = (typeInput) => {
+  return new Promise( async (resolve, reject) => {
+      try {
+        if(!typeInput) { // trường hợp không chạy vào input
+          resolve({
+            errCode: 1,
+            errMessage: 'Thiếu tham số bắt buộc !',
+          })
+        } else {
+          let res = {}; // định nghĩa biến res
+          let allcode = await db.Allcode.findAll({  //gọi tất cả
+            where: { type: typeInput}
+          }); 
+          res.errCode = 0; //key của nó là errCode
+          res.data = allcode;
+          resolve(res);
+        }
+      } catch(e) {
+        reject(e)
+      }
+  })
+}
+
+module.exports = {
+  handleUserLogin: handleUserLogin,
+  getAllUsers: getAllUsers,
+  createNewUser: createNewUser,
+  deleteUser: deleteUser,
+  updateUserData: updateUserData,
+  getAllCodeService: getAllCodeService,
+};
+
