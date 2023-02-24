8d1486cde54710da618259ee1cded0dd4679cb90
Manh-Truong <truonghuymanh20@gmail.com>
2023-02-24 21:22:12 +0700

    new

--- src/models/user.js
+++ src/models/user.js
@@ -10,25 +10,27 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      User.belongsTo(models.AllCode, { foreignKey: 'positionId', targetKey: 'keyMap', as: 'positionData'})
+      User.belongsTo(models.AllCode, { foreignKey: 'gender', targetKey: 'keyMap', as: 'genderData'})
     }
   };
-  User.init({
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    address: DataTypes.STRING,
-    phonenumber: DataTypes.STRING,
-    gender: DataTypes.STRING,
-    image: DataTypes.STRING,
-    roleId: DataTypes.STRING,
-    positionId: DataTypes.STRING,
-
-
-  }, {
-    sequelize,
-    modelName: 'User',
-  });
+  User.init(
+    {
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      firstName: DataTypes.STRING,
+      lastName: DataTypes.STRING,
+      address: DataTypes.STRING,
+      phonenumber: DataTypes.STRING,
+      gender: DataTypes.STRING,
+      image: DataTypes.STRING,
+      roleId: DataTypes.STRING,
+      positionId: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "User",
+    }
+  );
   return User;
 };
--- src/services/userService.js
+++ src/services/userService.js
@@ -129,7 +129,8 @@ let createNewUser = (data) => {
           phonenumber: data.phonenumber,
           gender: data.gender,
           roleId: data.roleId,
-          positionId: data.positionId
+          positionId: data.positionId,
+          image: data.avatar
       });
       resolve({
         errCode: 0,
@@ -168,7 +169,7 @@ let deleteUser = (userId) => {
 let updateUserData = (data) => {
   return new Promise(async (resolve, reject) => {
     try {
-        if(!data.id){
+        if(!data.id || !data.roleId || !data.positionId || !data.gender){
             resolve({
                 errCode: 2,
                 errMessage: 'Thiếu thông số bắt buộc'
@@ -183,8 +184,13 @@ let updateUserData = (data) => {
             user.firstName = data.firstName;
             user.lastName = data.lastName;
             user.address = data.address;
+            user.roleId = data.roleId;
+            user.positionId = data.positionId;
+            user.gender = data.gender;
             user.phonenumber = data.phonenumber;
-
+            if (data.avatar){
+              user.image = data.avatar;
+            }
           await user.save();
 
         resolve({
